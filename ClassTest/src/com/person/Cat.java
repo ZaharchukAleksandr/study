91206ba21462fa226559e189f6c514d7ac33758e
Zaharchuk Aleksandr <zaharchuk.aleksandr@gmail.com>
2019-11-07 16:28:30 +0200

    construktors and ...

---
+++ ClassTest/src/com/person/Cat.java
@@ -0,0 +1,25 @@
+package com.person;
+
+class Cat
+{
+	 
+	 private String name;
+	 private String color;
+	 
+	 public String getName(){
+	      return name;
+	 } 
+	 
+	 public void setName(String a){
+	      name = a;
+	 }
+	 
+	 public String getColor(){
+	      return color;
+	 } 
+	 
+	 public void setColor(String color){
+	      this.color = color;
+	 }
+	 
+}
