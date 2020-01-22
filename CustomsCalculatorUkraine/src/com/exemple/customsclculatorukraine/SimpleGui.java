3fe20b36a80dac820b5db7fc768e51357cf5df70
Zaharchuk Aleksandr <zaharchuk.aleksandr@gmail.com>
2020-01-22 16:26:28 +0200

    add gui

--- CustomsCalculatorUkraine/src/com/exemple/customsclculatorukraine/SimpleGui.java
+++ CustomsCalculatorUkraine/src/com/exemple/customsclculatorukraine/SimpleGui.java
@@ -1,6 +1,6 @@
 package com.exemple.customsclculatorukraine;
 
-import java.awt.Container;
+import java.awt.*;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.util.Calendar;
@@ -11,29 +11,43 @@ public class SimpleGui extends JFrame {
 	
 
 	
-	private JTextField priceTf;
-	private JTextField capacityTf;
-	private JTextField yearTf;
+	private JTextField priceTf = new JTextField();
+	private JTextField capacityTf = new JTextField();
+	private JTextField yearTf = new JTextField();
 	
-	private JLabel priceLbl;
-	private JLabel capacityLbl;
-	private JLabel yearLbl;
+	private JLabel priceLbl = new JLabel("Цена в евро");
+	private JLabel capacityLbl = new JLabel("Обьем двигателя");
+	private JLabel yearLbl = new JLabel("Год выпуска");
+	private JLabel free = new JLabel();
 
-	private JRadioButton currencyRb;
-	private JRadioButton fuelRb;
+	private JRadioButton currencyRb = new JRadioButton();
+	private JRadioButton fuelRb = new JRadioButton("Gas");
 	
-	private JButton calcBtn;
+	private JButton calcBtn = new JButton("Расчет");
 	
 		public SimpleGui() {
 		this.setTitle("Калькулятор растаможки");
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setBounds(250, 250, 300, 250);
 		
-		Container container = this.getContentPane();
-		container.setLayout(new GridLayout(3,2,2,2));
-		container.add(priceTf);
-		container.add(capacityTf);
-		container.add(yearTf);
+		Container cont = this.getContentPane();
+		
+		cont.setLayout(new GridLayout(4, 3, 3, 3));
+		cont.add(priceLbl);
+		cont.add(priceTf);
+		cont.add(currencyRb);
+		cont.add(capacityLbl);
+		cont.add(capacityTf);
+		cont.add(fuelRb);
+		cont.add(yearLbl);
+		cont.add(yearTf);
+		cont.add(free);
+		cont.add(free);
+		cont.add(free);
+		cont.add(calcBtn);
+		
+		
+		
 		
 		
 		
