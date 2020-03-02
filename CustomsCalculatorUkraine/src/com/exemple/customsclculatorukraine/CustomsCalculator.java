7bb28e6353237ed8e37dc630dd04efad9f775592
Zaharchuk Aleksandr <zaharchuk.aleksandr@gmail.com>
2020-03-02 18:45:18 +0200

    restore castomCalculator

---
+++ CustomsCalculatorUkraine/src/com/exemple/customsclculatorukraine/CustomsCalculator.java
@@ -0,0 +1,30 @@
+package com.exemple.customsclculatorukraine;
+
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+
+public class CustomsCalculator extends JFrame{
+	
+	public static int engineCapacity;
+	public static int carPrice;
+	public static int productionYear;
+	
+
+	public static double kourse = 24.2527; 
+	public static int year = 2020;
+	public int tax = 10;
+	public int vat = 20;
+	
+	
+	public static void main (String[] args) {
+		
+		SimpleGui gui = new SimpleGui();
+		Benzin benz = new Benzin();
+	
+	
+
+		
+	}
+	
+}
